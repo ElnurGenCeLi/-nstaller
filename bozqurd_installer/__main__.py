bff0b0c877476a743aebe6182910fab11d4cbfaa
Aykhan026 <78858146+aykhan026@users.noreply.github.com>
2021-10-06 00:45:50 +0400

    Update __main__.py

--- bozqurd_installer/__main__.py
+++ bozqurd_installer/__main__.py
@@ -154,7 +154,7 @@ if __name__ == "__main__":
     config['TMP_DOWNLOAD_DIRECTORY'] = "./downloads/"
     config['TZ'] = TZ
     config['TZ_NUMBER'] = "1"
-    config['UPSTREAM_REPO_URL'] = "https://github.com/aykhan026/bozqurd"
+    config['UPSTREAM_REPO_URL'] = "https://github.com/aykhan026/bozqurduserbot"
     config['SEVGILI'] = "None"
     config['WARN_LIMIT'] = "3"
     config['WARN_MODE'] = "gmute"
